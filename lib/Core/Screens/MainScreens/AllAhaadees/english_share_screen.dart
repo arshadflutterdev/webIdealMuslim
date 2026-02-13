12aba4fb054b20ae40201ef2d419cbc5b9f29964
arshadflutterdev <arshadcodess@gmail.com>
2026-02-13 12:38:52 +0500

    changes

---
+++ lib/Core/Screens/MainScreens/AllAhaadees/english_share_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EnglishShareScreen extends StatefulWidget {
+  const EnglishShareScreen({super.key});
+
+  @override
+  State<EnglishShareScreen> createState() => _EnglishShareScreenState();
+}
+
+class _EnglishShareScreenState extends State<EnglishShareScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
